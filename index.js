331038d7618cdd81f5fb1c0251df28fd40b8fa5f
m.kalugin <kalugin@jam-it.ru>
2025-07-21 15:10:20 +0400

    update index.js

--- index.js
+++ index.js
@@ -1,32 +1,92 @@
-let inventory = [
-    {id: 1, name: 'Apple', price: 0.50, quantity: 200},
-    {id: 2, name: 'Banana', price: 0.30, quantity: 150},
-    {id: 3, name: 'Apple', price: 0.90, quantity: 50},
-]
+// let inventory = [{
+//         id: 1,
+//         name: 'Apple',
+//         price: 0.50,
+//         quantity: 200
+//     },
+//     {
+//         id: 2,
+//         name: 'Banana',
+//         price: 0.30,
+//         quantity: 150
+//     },
+//     {
+//         id: 3,
+//         name: 'Apple',
+//         price: 0.90,
+//         quantity: 50
+//     },
+// ]
 
-// const productName = inventory.map((item) => item.name);
-// console.log(productName);
+// // const productName = inventory.map((item) => item.name);
+// // console.log(productName);
 
-function updateInventory(productId, amount) {
-    inventory.map((item) => {
-        item.id === productId ? item.quantity += amount : '';
-        item.quantity < 0 ? item.quantity = 0 : ''
-    })
+// function updateInventory(productId, amount) {
+//     inventory.map((item) => {
+//         item.id === productId ? item.quantity += amount : '';
+//         item.quantity < 0 ? item.quantity = 0 : ''
+//     })
+// }
+
+// updateInventory(1, -150)
+// updateInventory(2, -130)
+// updateInventory(3, -24)
+
+// function findStock(threshold) {
+//     inventory.map((item) => {
+//         if (item.quantity <= threshold) {
+//             console.log(item.name);
+//         }
+//     })
+// }
+
+// console.log(inventory);
+// findStock(60)
+
+
+// function totalInventoryValue() {
+//     let totalCost = 0;
+//     inventory.map((item) => {
+//         for (let i = 0; i < inventory.length; i++) {
+//             totalCost += item.price * item.quantity;
+//         }
+//         console.log(totalCost);
+//     })
+// }
+
+// totalInventoryValue()
+
+
+const students = {
+    Alice: {
+        math: 92,
+        physics: 87,
+        literature: 81
+    },
+    Bob: {
+        math: 75,
+        physics: 90,
+        literature: 88
+    },
+    Charlie: {
+        math: 92,
+        physics: 93,
+        literature: 85
+    },
+    Diana: {
+        math: 89,
+        physics: 91,
+        literature: 90
+    },
+};
+    
+function getTopStudent(subject, studentsObj) {
+    const studentsArr = (Object.entries(students));   
+    studentsArr.forEach(([name, lessons]) => {       
+        console.log(Object.entries(lessons));
+    })    
+    
 }
 
-updateInventory(1, -150)
-updateInventory(2, -130)
-updateInventory(3, -24)
 
-function findStock(threshold) {
-    Object.entries(inventory).forEach((item) => {
-        console.log(item);
-    })
-
-
-}
-
-findStock(35)
-
-
-
+getTopStudent();
